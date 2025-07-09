2fa57ffe2b734ee71409456022d7669a8f298075
Mohitkanyal <102403245+Mohitkanyal@users.noreply.github.com>
2025-07-09 19:15:41 +0530

    Create SummaryApi.js

---
+++ frontend/src/common/SummaryApi.js
@@ -0,0 +1,12 @@
+const BASE_URL = "https://roc8-fn7b.onrender.com";
+
+const SummaryApi = {
+  salaryDetails: {
+    url: `${BASE_URL}/predict_salary`,
+  },
+  courseRecommendation: {
+    url: `${BASE_URL}/recommend_course`,
+  }
+};
+
+export default SummaryApi;
